e8d82dc3136ad299de1ea17ecc079032c993bf52
Starix <646722505@qq..com>
2020-07-07 02:19:31 +0800

    httpclient进一步封装，可操作cookie，并将单例httpclient改为非单例

--- src/main/java/com/starix/gdou/service/impl/GdouJWServiceimplV2.java
+++ src/main/java/com/starix/gdou/service/impl/GdouJWServiceimplV2.java
@@ -13,15 +13,10 @@ import com.starix.gdou.service.GdouJWServiceV2;
 import com.starix.gdou.utils.HttpClientUtil;
 import com.starix.gdou.utils.RSAUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.Consts;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.security.interfaces.RSAKey;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -46,20 +41,29 @@ public class GdouJWServiceimplV2 implements GdouJWServiceV2 {
     private String WEBVPN_PASSWORD;
 
 
+
     @Override
     public LoginResultV2 login(String useranme, String password) throws Exception {
-        // doWebvpnLogin();
+        HttpClientUtil httpClient = new HttpClientUtil();
+        // 一次完整流程中的所有请求都需要使用同一个httpclient实例，保持cookie同步
+        doWebvpnLogin(httpClient);
         // 获取publickey，用于密码rsa加密
-        String resultStr = HttpClientUtil.doGet(JW_URL + "/xtgl/login_getPublicKey.html");
+        String resultStr = httpClient.doGet(JW_URL + "/xtgl/login_getPublicKey.html");
         JSONObject json = JSON.parseObject(resultStr);
         String modulus = json.getString("modulus");
         String exponent = json.getString("exponent");
         // 密码加密
         String encryptPassword = RSAUtil.encrypt(modulus, exponent, password);
-        System.out.println(encryptPassword);
-        // 如果当前请求直接进入教务系统首页，说明是已登录状态，无需再登录
+        // System.out.println(encryptPassword);
+        // 如果当前请求直接重定向进入教务系统首页，说明是已登录状态，无需再登录
         // 当评价执行过程出现异常导致webvpn未正确注销时，相同账号再次登录教务系统就会出现这个情况
-        HttpResponse response = HttpClientUtil.doGetAndGetResponse(JW_URL + "/xtgl/login_slogin.html");
+        resultStr = httpClient.doGet(JW_URL + "/xtgl/login_slogin.html");
+        // TODO: 2020-07-07 教务系统登录待完成
+        System.out.println(httpClient.getCookie("wengine_vpn_ticketwebvpn_gdou_edu_cn"));
+        if (!resultStr.contains("用户登录")){
+            System.out.println(resultStr);
+            System.out.println("用户已登录");
+        }
         return null;
     }
 
@@ -67,21 +71,22 @@ public class GdouJWServiceimplV2 implements GdouJWServiceV2 {
     /**
      * 登录webvpn，获取cookie（HttpClient会自己维护cookie）
      * @throws Exception
+     * @param httpClient
      */
-    private void doWebvpnLogin() throws Exception {
+    private void doWebvpnLogin(HttpClientUtil httpClient) throws Exception {
         Map<String, String> params = new HashMap<>();
         params.put("auth_type", "local");
         params.put("username", WEBVPN_USERNAME);
         params.put("sms_code", "");
         params.put("password", WEBVPN_PASSWORD);
         params.put("needCaptcha", "false");
-        String resultStr = HttpClientUtil.doPost(WEBVPN_URL + "/do-login?local_login=true", params);
+        String resultStr = httpClient.doPost(WEBVPN_URL + "/do-login?local_login=true", params);
         JSONObject json = JSON.parseObject(resultStr);
         if (!json.getBoolean("success")){
             if ("NEED_CONFIRM".equals(json.getString("error"))){
                 // 当前需要确认登录
                 log.info("踢掉已登录的webvpn客户端");
-                doWebvpnConfirmLogin();
+                doWebvpnConfirmLogin(httpClient);
             }else {
                 log.error("内置webvpn异常：{}", resultStr);
                 throw new CustomException(CommonResult.failed("内置webvpn异常，等待修复"));
@@ -91,9 +96,10 @@ public class GdouJWServiceimplV2 implements GdouJWServiceV2 {
 
     /**
      * 执行确认登录，踢掉已登录的webvpn客户端
+     * @param httpClient
      */
-    private void doWebvpnConfirmLogin() throws IOException {
-        String resultStr = HttpClientUtil.doPost(WEBVPN_URL + "/do-confirm-login", new HashMap<>());
+    private void doWebvpnConfirmLogin(HttpClientUtil httpClient) throws IOException {
+        String resultStr = httpClient.doPost(WEBVPN_URL + "/do-confirm-login", new HashMap<>());
         JSONObject json = JSON.parseObject(resultStr);
         if (!json.getBoolean("success")){
             throw new CustomException(CommonResult.failed("内置webvpn异常，等待修复"));
